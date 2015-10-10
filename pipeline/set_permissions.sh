bbdd99b7dc949961e4db85cbff4fd287b9314a5c
robbert <robbert@vdzon.com>
2015-10-10 20:39:54 +0200

    update

--- pipeline/set_permissions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-chown -R 200:200 data/nexus
-chown -R 1000:1000 data/jenkins
-chown -R 102:105 data/redis
-chown -R 1000:1000 data/gitlab
-chown -R 102:106 data/postgresql
-
---
+++ start_pipeline.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+chown -R 1000:1000 .
+chown -R 200:200 pipeline/data/nexus
+
